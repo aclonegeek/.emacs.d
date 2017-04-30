8d5be8d6066bcfa4a43a97d45f32203f4924e80e
Randy Taylor <tehgecKozzz@gmail.com>
2017-04-30 00:53:59 -0400

    Improve startup time by reducing calls to gc on launch

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+(run-with-idle-timer
+ 5 nil
+ (lambda ()
+   (setq gc-cons-threshold 1000000)
+   (message "gc-cons-threshold restored to %S"
+            gc-cons-threshold)))
+
 (require 'package)
 
 (add-to-list 'package-archives
