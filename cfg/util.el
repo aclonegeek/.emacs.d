cc3001bea82963da1e3f7ef92fce6c4a23929f13
Randy Taylor <tehgecKozzz@gmail.com>
2017-07-16 16:33:22 -0400

    Add util.el to store custom functions
    Auto load darktooth-modeline-three
    
    More use-package utilizations

---
+++ cfg/util.el
@@ -0,0 +1,60 @@
+(defun iwb ()
+  "Indent whole buffer."
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
+
+(defun revert-buffer-no-comfirm ()
+  "Revert buffer without confirmation."
+  (interactive)
+  (revert-buffer :ignore-auto :noconfirm))
+
+(defun duplicate-line ()
+  "Copy the current line and paste it on to a new line."
+  (interactive)
+  (kill-whole-line)
+  (yank)
+  (yank)
+  (forward-line -1))
+
+(defun select-word ()
+  "Select the current word."
+  (interactive)
+  (backward-word)
+  (set-mark (point))
+  (forward-word))
+
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+(defun find-other-file-other-window ()
+  "Find the corresponding file in another window."
+  (interactive)
+  (if (not (one-window-p))
+      (delete-other-windows))
+  (split-window-right)
+  (other-window 1)
+  (ff-find-other-file))
+
+(defun reload-config()
+  "Reload emacs config."
+  (interactive)
+  (load-file "~/.emacs.d/init.el"))
+
+(defun edit-config()
+  "Edit emacs config."
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(provide 'util)
--- init.el
+++ init.el
@@ -26,22 +26,22 @@
 (setq use-package-always-ensure t)
 
 (use-package adsc :load-path "adsc/"
-  :bind ("<f5>" . adsc-todo)
-  :bind ("<f6>" . adsc-python-script)
-  :bind ("<f7>" . adsc-python-module)
-  :bind ("<f9>" . adsc-file-created)
-  :bind ("C-<f9>" . adsc-last-update)
+  :bind ("<f5>"     . adsc-todo)
+  :bind ("<f6>"     . adsc-python-script)
+  :bind ("<f7>"     . adsc-python-module)
+  :bind ("<f9>"     . adsc-file-created)
+  :bind ("C-<f9>"   . adsc-last-update)
   :bind ("C-S-<f9>" . adsc-update-history))
 
 (use-package keybinds :load-path "cfg/")
 
-(use-package ui :load-path "ui/")
+(use-package util :load-path "cfg/")
 
-;; (use-package config :load-path "lang/python/")
+(use-package ui :load-path "ui/")
 
 (use-package python-mode
   :mode "\\.py\\'"
-  :interpreter ("python" . python-mode)
+  :interpreter ("python")
   :init
   (add-hook 'python-mode-hook (lambda ()
                                 (flycheck-mode 1)
@@ -50,7 +50,8 @@
                                       flycheck-checker-error-threshold 400
                                       flycheck-pylintrc "C:/Users/Randy/.pylintrc")))
   :config
-  (setq-default flycheck-disabled-checkers '(python-flake8))
+  (setq-default flycheck-disabled-checkers '(python-flake8
+					     emacs-lisp-checkdoc))
   (defvar python-check-command "pylint"))
 
 (use-package auto-complete
@@ -67,6 +68,8 @@
   :bind ("<f12>" . pylint-insert-ignore-comment))
 
 (use-package darktooth-theme
+  :init
+  (add-hook 'after-init-hook #'darktooth-modeline-three)
   :config
   (load-theme 'darktooth t))
 
@@ -82,9 +85,16 @@
   :bind ("<f8>" . neotree-toggle))
 
 (use-package projectile
-  :init (add-hook 'python-mode-hook 'projectile-mode))
+  :config (add-hook 'python-mode-hook 'projectile-mode))
 
-(use-package flx-ido)
+(use-package flx-ido
+  :init
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (flx-ido-mode 1)
+  :config
+  (setq ido-enable-flex-matching t)
+  (setq ido-use-faces nil))
 
 (use-package rainbow-delimiters
   :config (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
@@ -94,65 +104,8 @@
   (setq sml/no-confirm-load-theme t)
   (sml/setup))
 
-(defun iwb ()
-  "Indent whole buffer."
-  (interactive)
-  (delete-trailing-whitespace)
-  (indent-region (point-min) (point-max) nil)
-  (untabify (point-min) (point-max)))
-
 ;; Tab auto-indent
 (setq tab-always-indent 'complete)
-
-;; Revert buffer
-(defun revert-buffer-no-comfirm ()
-  "Revert buffer without confirmation."
-  (interactive)
-  (revert-buffer :ignore-auto :noconfirm))
-
-(defun duplicate-line ()
-  "Copy the current line and paste it on to a new line."
-  (interactive)
-  (kill-whole-line)
-  (yank)
-  (yank)
-  (forward-line -1))
-
-(defun select-word ()
-  "Select the current word."
-  (interactive)
-  (backward-word)
-  (set-mark (point))
-  (forward-word))
-
-(defun rename-file-and-buffer ()
-  "Rename the current buffer and file it is visiting."
-  (interactive)
-  (let ((filename (buffer-file-name)))
-    (if (not (and filename (file-exists-p filename)))
-        (message "Buffer is not visiting a file!")
-      (let ((new-name (read-file-name "New name: " filename)))
-        (cond
-         ((vc-backend filename) (vc-rename-file filename new-name))
-         (t
-          (rename-file filename new-name t)
-          (set-visited-file-name new-name t t)))))))
-
-;; flx-ido
-(ido-mode 1)
-(ido-everywhere 1)
-(flx-ido-mode 1)
-(setq ido-enable-flex-matching t)
-(setq ido-use-faces nil)
-
-(defun find-other-file-other-window ()
-  "Find the corresponding file in another window."
-  (interactive)
-  (if (not (one-window-p))
-      (delete-other-windows))
-  (split-window-right)
-  (other-window 1)
-  (ff-find-other-file))
 
 ;; Disable backup files
 (setq make-backup-files nil)
@@ -160,16 +113,6 @@
 ;; 4 spaces instead of tabs
 (setq indent-tabs-mode nil)
 (setq-default c-basic-offset 4)
-
-(defun reload-config()
-  "Reload emacs config."
-  (interactive)
-  (load-file "~/.emacs.d/init.el"))
-
-(defun edit-config()
-  "Edit emacs config."
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
