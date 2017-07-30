41a4a0c5d9825aab9c2d7dcc146cfd68ee6e4c04
Randy Taylor <tehgecKozzz@gmail.com>
2017-07-29 22:57:26 -0400

    Define font (Droid Sans Mono) in ui.el

--- ui/ui.el
+++ ui/ui.el
@@ -4,6 +4,11 @@
 (scroll-bar-mode -1)
 (horizontal-scroll-bar-mode -1)
 
+;; Font
+(set-face-attribute 'default nil
+		    :family "Droid Sans Mono"
+		    :height 95)
+
 ;; Highlight current line
 (global-hl-line-mode t)
 
