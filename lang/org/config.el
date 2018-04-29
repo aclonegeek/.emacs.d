0cff5b716e0fe264bdc87fa45ab7a86f07a8ecf7
Randy Taylor <tehgecKozzz@gmail.com>
2018-04-28 21:20:39 -0400

    Update org config

--- lang/org/config.el
+++ lang/org/config.el
@@ -1,8 +1,15 @@
 ;; -*- lexical-binding: t; -*-
 
-(use-package org-bullets
+;;(use-package org-bullets
+;;  :ensure t
+;;  :config
+;;  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 0))))
+
+;; Wrap lines in org-mode
+(setq org-startup-truncated nil)
+
+(use-package org-preview-html
   :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 0))))
+  :bind ("C-c o p" . org-preview-html-mode))
 
 (provide 'config)
