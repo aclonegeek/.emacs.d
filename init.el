54ffe7d9cbac397a490d9e58e2b58f4f72202e3c
Randy Taylor <tehgecKozzz@gmail.com>
2018-05-04 21:43:16 -0400

    Add counsel-projectile and enable full abbreviation for ivy virtual buffers

--- init.el
+++ init.el
@@ -102,16 +102,23 @@
   :ensure t
   :config
   (ivy-mode t)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d/%d) ")
-  (setq ivy-re-builders-alist
+  (setq ivy-use-virtual-buffers t
+        ivy-count-format "(%d/%d) "
+        ivy-re-builders-alist
         '((swiper . ivy--regex-plus)
-          (t      . ivy--regex-fuzzy))))
+          (t      . ivy--regex-fuzzy))
+        ivy-virtual-abbreviate 'full))
 
 (use-package counsel
   :ensure t
   :after ivy
   :bind ("M-x" . counsel-M-x))
+
+(use-package counsel-projectile
+  :ensure t
+  :after counsel
+  :config
+  (counsel-projectile-mode))
 
 (use-package swiper
   :ensure t
