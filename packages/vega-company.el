cffa2dd002f65b2117ecd01446d52d9a107e35c7
Randy Taylor <tehgecKozzz@gmail.com>
2018-05-07 21:15:10 -0400

    Defer loading company for 1 second

--- packages/vega-company.el
+++ packages/vega-company.el
@@ -2,10 +2,11 @@
 
 (use-package company
   :ensure t
+  :defer 1
   :commands company-mode
   :config
-  (setq company-idle-delay nil)
-  (setq company-minimum-prefix-length 1)
+  (setq company-idle-delay nil
+        company-minimum-prefix-length 1)
   (global-company-mode 1))
 
 (provide 'vega-company)
