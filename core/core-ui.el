f740031ba7b03df2ea00db96b2ea7ef96d60224f
Randy Taylor <tehgecKozzz@gmail.com>
2018-05-09 21:59:01 -0400

    Add supposed performance optimizations and disable mode-line hover tips

--- core/core-ui.el
+++ core/core-ui.el
@@ -10,6 +10,15 @@
 (global-hl-line-mode t)          ;; Highlight current line
 (show-paren-mode)                ;; Show matching parentheses
 
+(setq-default
+ ;; Performance optimizations
+ bidi-display-reordering nil
+ jit-lock-defer-time nil
+ jit-lock-stealth-nice 0.1
+ jit-lock-stealth-time 0.2
+ jit-lock-stealth-verbose nil
+ mode-line-default-help-echo nil) ;; Disable mode-line hover tips
+
 ;; Font
 (set-face-attribute 'default nil
                     :family "Noto Mono"
