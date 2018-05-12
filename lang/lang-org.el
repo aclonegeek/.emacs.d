108189189dfb4eb475383a2d1b6e7ab9c16f2748
Randy Taylor <tehgecKozzz@gmail.com>
2018-05-12 18:54:53 -0400

    Unbind C-k and C-x n e from org-mode, and wrap org-mode configurations in use-package

--- lang/lang-org.el
+++ lang/lang-org.el
@@ -1,15 +1,16 @@
 ;; -*- lexical-binding: t; -*-
 
-;;(use-package org-bullets
-;;  :ensure t
-;;  :config
-;;  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 0))))
-
-;; Wrap lines in org-mode
-(setq org-startup-truncated nil)
+(use-package org
+  :defer t
+  :config
+  (unbind-key "C-k" org-mode-map)
+  (unbind-key "C-x n e" org-mode-map)
+  ;; Wrap lines in org-mode
+  (setq org-startup-truncated nil))
 
 (use-package org-preview-html
   :ensure t
+  :defer t
   :bind ("C-c o p" . org-preview-html-mode))
 
 (provide 'lang-org)
