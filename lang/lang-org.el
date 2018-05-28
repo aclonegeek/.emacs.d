7f20d8778382691c68f1946e258a5b65fae2e49a
Randy Taylor <tehgecKozzz@gmail.com>
2018-05-27 21:02:28 -0400

    Remove org-preview-html package
    Delete todo.org

--- lang/lang-org.el
+++ lang/lang-org.el
@@ -8,9 +8,4 @@
   ;; Wrap lines in org-mode
   (setq org-startup-truncated nil))
 
-(use-package org-preview-html
-  :ensure t
-  :defer t
-  :bind ("C-c o p" . org-preview-html-mode))
-
 (provide 'lang-org)
