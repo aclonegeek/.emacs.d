ddbd1f12f36f85383992ec520be69207702057ab
Randy Taylor <tehgecKozzz@gmail.com>
2018-05-30 20:47:25 -0400

    Minor tweaks to core-settings

--- core/core-settings.el
+++ core/core-settings.el
@@ -2,17 +2,23 @@
 
 (setq inhibit-startup-screen t
       initial-scratch-message "")
+
 (fset 'yes-or-no-p 'y-or-n-p)
+
+;; Coding systems.
 (prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
 
-;; Disable backups and autosaves
-(setq-default make-backup-files nil
-              auto-save-default nil)
-
-;; 4 spaces instead of tabs
-(setq-default indent-tabs-mode nil
-              tab-width 4
-              tab-stop-list (quote (4 8))
-              tab-always-indent 'complete)
+(setq-default
+ ring-bell-function 'ignore
+ visible-bell nil
+ ;; Disable backups and autosaves
+ make-backup-files nil
+ auto-save-default nil
+ ;; 4 spaces instead of tabs
+ indent-tabs-mode nil
+ tab-width 4
+ tab-stop-list (quote (4 8))
+ tab-always-indent 'complete)
 
 (provide 'core-settings)
