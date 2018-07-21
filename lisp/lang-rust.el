f1b6b850ad14658dee921b77bed2944d9db46b9e
Randy Taylor <tehgecKozzz@gmail.com>
2018-07-20 21:14:49 -0400

    rust-mode -> rustic-mode
    Remove old commented settings

--- lisp/lang-rust.el
+++ lisp/lang-rust.el
@@ -1,45 +1,22 @@
 ;; -*- lexical-binding: t; -*-
 
-(use-package rust-mode
+(use-package rustic
   :ensure t
   :defer t
-  :commands rust-mode
+  :commands rustic-mode
   :mode "\\.rs\\'"
   :config
-  (setq rust-format-on-save t))
-
-(use-package cargo
-  :ensure t
-  :defer t
-  :after rust-mode
-  :hook (rust-mode . cargo-minor-mode))
-
-;;(use-package racer
-;;  :ensure t
-;;  :defer t
-;;  :after rust-mode
-;;  :hook ((rust-mode . racer-mode)
-;;         (racer-mode . eldoc-mode))
-;;  :config
-;;  (setq racer-cmd "C:/Users/Randy/.cargo/bin/racer"
-;;        racer-rust-src-path "C:/Users/Randy/Programming/Code/rust/src"))
-;;
-;;(use-package company-racer
-;;  :ensure t
-;;  :defer t
-;;  :after racer
-;;  :config
-;;  (add-to-list 'company-backends 'company-racer))
+  (setq rustic-format-on-save t))
 
 (use-package flycheck-rust
   :ensure t
   :defer t
-  :after (flycheck rust-mode)
+  :after (flycheck rustic-mode)
   :hook (flycheck-mode . flycheck-rust-setup))
 
 (use-package lsp-rust
   :ensure t
-  :hook (rust-mode . lsp-rust-enable)
+  :hook (rustic-mode . lsp-rust-enable)
   :config
   (setq lsp-rust-rls-command '("rustup" "run" "nightly" "rls")))
 
