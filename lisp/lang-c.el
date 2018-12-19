7ff4cd66c5aef9d56819356da4cc2cc1b1d7e972
Randy Taylor <tehgecKozzz@gmail.com>
2018-12-19 15:46:52 -0500

    Use ccls for C and C++ programming

--- lisp/lang-c.el
+++ lisp/lang-c.el
@@ -2,4 +2,10 @@
 
 (setq-default c-basic-offset 4)
 
+(use-package ccls
+  :ensure t
+  :defer t
+  :hook ((c-mode   . (lambda () (require 'ccls)(lsp)))
+         (c++-mode . (lambda () (require 'ccls)(lsp)))))
+
 (provide 'lang-c)
