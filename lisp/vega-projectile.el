dd5e80487bfde5231f1f0e95286a4fd6a99d9cf6
Randy Taylor <tehgecKozzz@gmail.com>
2019-01-19 21:18:29 -0500

    Set projectile-generic-command to use fd

--- lisp/vega-projectile.el
+++ lisp/vega-projectile.el
@@ -5,9 +5,10 @@
   :bind-keymap ("C-c p" . projectile-command-map)
   :hook (prog-mode . projectile-mode)
   :config
-  (setq projectile-enable-caching t
-        projectile-indexing-method 'alien
-        projectile-completion-system 'ivy)
+  (setq projectile-enable-caching    t
+        projectile-indexing-method   'alien
+        projectile-completion-system 'ivy
+        projectile-generic-command   "fd . -0")
   (add-to-list 'projectile-globally-ignored-directories ".ccls-cache"))
 
 (use-package counsel-projectile
