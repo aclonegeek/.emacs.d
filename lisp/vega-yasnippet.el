15405decdfc739ec315aa2663582418e3a77d86f
Randy Taylor <tehgecKozzz@gmail.com>
2019-05-13 20:17:59 -0400

    Remove unnecessary config from yasnippet

--- lisp/vega-yasnippet.el
+++ lisp/vega-yasnippet.el
@@ -3,8 +3,6 @@
 (use-package yasnippet
   :ensure t
   :commands (yas-minor-mode yas-reload-all)
-  :hook (prog-mode . yas-minor-mode)
-  :config
-  (yas-reload-all))
+  :hook (prog-mode . yas-minor-mode))
 
 (provide 'vega-yasnippet)
