e31c79c79bdc29922d0d2cc44e22305ff03f5554
Randy Taylor <tehgecKozzz@gmail.com>
2019-05-22 19:18:36 -0400

    Tidy up keybinds

--- lisp/core-keybinds.el
+++ lisp/core-keybinds.el
@@ -2,19 +2,19 @@
 
 (global-set-key [f1] 'shell)
 (global-set-key [f2] 'shell-command)
-;; Mirror most popular text editor's C-backspace functionality (for the most part)
-(global-set-key [C-backspace] 'backward-kill-word)
+
+;; Mirror most popular text editor's C-backspace functionality (for the most part).
+(global-set-key [C-backspace] 'backward-delete-word)
 (global-set-key [C-tab] 'goto-line)
 
-(global-set-key (kbd "C-c C-a") 'align)
 (global-set-key (kbd "C-c a") 'align-regexp)
 
-(global-set-key (kbd "<C-backspace>") 'backward-delete-word)
 (global-set-key (kbd "M-d") 'delete-word)
 (global-set-key (kbd "C-k") 'kill-whole-line)
 (global-set-key (kbd "M-k") 'kill-line)
 (global-set-key (kbd "C-z") 'duplicate-line)
-(global-set-key (kbd "C-q") 'select-word)
+(global-set-key (kbd "C-q") 'select-word-backward)
+(global-set-key (kbd "M-q") 'select-word-forward)
 
 (global-set-key (kbd "C-c r") 'revert-buffer-no-comfirm)
 (global-set-key (kbd "C-c n") 'rename-file-and-buffer)
