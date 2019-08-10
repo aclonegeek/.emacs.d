841b062d640cba61ec7fba3a15600eaa50be41c7
Randy Taylor <tehgecKozzz@gmail.com>
2019-08-09 21:02:20 -0400

    Return to old theme from bb4434bd

--- lisp/core-ui.el
+++ lisp/core-ui.el
@@ -25,16 +25,22 @@
                     :height 110)
 
 ;; Theme
-(use-package base16-theme
+(use-package doom-themes
   :ensure t
   :config
-  (load-theme 'base16-atelier-dune t)
-  (set-face-attribute 'mode-line           nil :background "#2D2B28")
-  (set-face-attribute 'region              nil :background "#3F3E36")
-  (set-face-attribute 'secondary-selection nil :background "#3F3E36")
-  (eval-after-load 'company
-    '(progn
-       (set-face-attribute 'company-tooltip-selection nil :background "#292824"))))
+  (load-theme 'doom-one t))
+
+;; https://gist.github.com/huytd/6b785bdaeb595401d69adc7797e5c22c
+(custom-set-faces
+ '(default ((t (:inherit nil :stipple nil :background "#212121" :foreground "#eeffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal :foundry "nil"))))
+ '(font-lock-constant-face ((t (:foreground "#C792EA"))))
+ '(font-lock-keyword-face ((t (:foreground "#2BA3FF" :slant italic))))
+ '(font-lock-preprocessor-face ((t (:inherit bold :foreground "#2BA3FF" :slant italic :weight normal))))
+ '(font-lock-string-face ((t (:foreground "#C3E88D"))))
+ '(font-lock-type-face ((t (:foreground "#FFCB6B"))))
+ '(font-lock-variable-name-face ((t (:foreground "#FF5370"))))
+ '(mode-line ((t (:background "#191919" :box nil))))
+ '(mode-line-inactive ((t (:background "#282828" :foreground "#5B6268" :box nil)))))
 
 ;; Modeline
 (use-package core-modeline
