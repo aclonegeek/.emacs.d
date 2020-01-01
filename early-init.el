e6e139c37c5451389d3bd981a75e8ca53ca95cf7
Randy Taylor <tehgecKozzz@gmail.com>
2020-01-01 17:06:59 -0500

    Move some stuff from init to early-init

--- early-init.el
+++ early-init.el
@@ -1,13 +1,27 @@
 ;; -*- lexical-binding: t; -*-
 
+(eval-and-compile
+  (add-to-list 'load-path "~/.emacs.d/lisp"))
+
+(defvar file-name-handler-alist-old file-name-handler-alist)
 (setq
+ site-run-file           nil
+ file-name-handler-alist nil
  ;; Disable autosaves.
  auto-save-default nil
  ;; Defer GC.
  gc-cons-threshold  most-positive-fixnum
  gc-cons-percentage 0.6
  ;; Don't resize the Emacs frame when GUI elements (e.g. bars, font) change.
- frame-inhibit-implied-resize t)
+ frame-inhibit-implied-resize t
+ ;; Don't use outdated bytecode.
+ load-prefer-newer t)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq file-name-handler-alist file-name-handler-alist-old
+                  gc-cons-threshold       16777216
+                  gc-cons-percentage      0.1)))
 
 ;; Package.
 (setq-default package-user-dir "~/.emacs.d/elpa"
--- init.el
+++ init.el
@@ -1,22 +1,5 @@
 ;; -*- lexical-binding: t; -*-
 
-;; Speedier startup.
-(defvar file-name-handler-alist-old file-name-handler-alist)
-(setq file-name-handler-alist nil
-      site-run-file           nil)
-
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (setq file-name-handler-alist file-name-handler-alist-old
-                  gc-cons-threshold       16777216
-                  gc-cons-percentage      0.1)))
-
-;; We don't want to use old .elc files.
-(setq load-prefer-newer t)
-
-(eval-and-compile
- (add-to-list 'load-path "~/.emacs.d/lisp"))
-
 (eval-when-compile
   ;; Ensure use-package is installed.
   (unless (locate-library "use-package")
