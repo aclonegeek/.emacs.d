dcad84823bfe31dabdb91e09e9fa5b4cf7be5398
Randy Taylor <tehgecKozzz@gmail.com>
2020-01-01 15:22:57 -0500

    Update rust config, remove flycheck-rust

--- lisp/lang-rust.el
+++ lisp/lang-rust.el
@@ -3,12 +3,7 @@
 (use-package rustic
   :commands rustic-mode
   :config
-  (require 'lsp-mode)
   (setq rustic-lsp-server     'rust-analyzer
-        rustic-format-on-save t))
-
-(use-package flycheck-rust
-  :after (flycheck rustic-mode)
-  :hook (flycheck-mode . flycheck-rust-setup))
+        rustic-format-trigger 'on-save))
 
 (provide 'lang-rust)
