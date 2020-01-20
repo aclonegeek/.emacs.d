7e58c96ff1c2cd84a5d060e853ecce6db074ff87
Randy Taylor <tehgecKozzz@gmail.com>
2020-01-20 15:29:27 -0500

    Clean up pkg-flycheck

--- lisp/pkg-flycheck.el
+++ lisp/pkg-flycheck.el
@@ -1,15 +1,13 @@
-;; -*- lexical-binding: t; -*- 
+;; -*- lexical-binding: t; -*-
 
 (use-package flycheck
   :ensure t
   :bind (("C-\\" . flycheck-list-errors)
          ("M-["  . flycheck-previous-error)
          ("M-]"  . flycheck-next-error))
-  :hook ((emacs-lisp-mode . flycheck-mode)
-         (python-mode     . flycheck-mode))
+  :hook (emacs-lisp-mode . flycheck-mode)
   :config
-  (setq-default flycheck-checker-error-threshold 400
-                flycheck-disabled-checkers '(emacs-lisp-checkdoc)
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)
                 flycheck-check-syntax-automatically '(save mode-enabled)))
 
 (provide 'pkg-flycheck)
