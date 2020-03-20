526dcab573701b1f037bee65530f6376f9a77b88
Randy Taylor <tehgecKozzz@gmail.com>
2020-03-19 20:42:42 -0400

    Keep company-capf backend

--- lisp/pkg-company.el
+++ lisp/pkg-company.el
@@ -6,7 +6,6 @@
   :hook (prog-mode . company-mode)
   :config
   (setq company-backends (delete 'company-dabbrev company-backends)
-        company-backends (delete 'company-capf company-backends)
         company-idle-delay 0.1
         company-minimum-prefix-length 1
         company-tooltip-limit 10
