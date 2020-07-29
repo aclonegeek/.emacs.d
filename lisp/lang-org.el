63e8bd4af0454c1058de24946f0b32b801a40bff
Randy Taylor <tehgecKozzz@gmail.com>
2020-07-29 08:36:55 -0400

    Unset org-latex-toc-command

--- lisp/lang-org.el
+++ lisp/lang-org.el
@@ -7,8 +7,6 @@
   (unbind-key "C-k" org-mode-map)
   (unbind-key "C-x n e" org-mode-map)
   ;; Wrap lines in org-mode
-  (setq org-startup-truncated nil)
-  ;; Insert a page break after the TOC.
-  (setq org-latex-toc-command "\\tableofcontents \\clearpage"))
+  (setq org-startup-truncated nil))
 
 (provide 'lang-org)
