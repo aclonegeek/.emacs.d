6601b1d200a281c899e7ac73167847c91b3c933f
Randy Taylor <tehgecKozzz@gmail.com>
2020-09-08 22:22:39 -0400

    Only load yasnippet for c/c++

--- lisp/pkg-yasnippet.el
+++ lisp/pkg-yasnippet.el
@@ -3,7 +3,6 @@
 (use-package yasnippet
   :ensure t
   :commands (yas-reload-all yas-minor-mode)
-  :hook ((after-init . (lambda () (yas-reload-all)))
-         (prog-mode  . (lambda () (yas-minor-mode)))))
+  :hook ((c-mode c++-mode) . yas-minor-mode))
 
 (provide 'pkg-yasnippet)
