326fd8d8c862ef9d130faf2440fab4d273f6f1f4
Randy Taylor <tehgecKozzz@gmail.com>
2020-09-08 23:00:56 -0400

    Remove lsp-python-ms and cleanup

--- lisp/lang-python.el
+++ lisp/lang-python.el
@@ -2,13 +2,6 @@
 
 (setq-default python-shell-interpreter "python3"
               python-indent-offset 4
-              python-indent-guess-indent-offset nil
-              ;; TODO(randy): Make this crossplatform.
-              flycheck-pylintrc "~/.flake8"
-              flycheck-pylintrc "~/.pylintrc")
-
-(use-package lsp-python-ms
-  :defer t
-  :hook (python-mode . (lambda () (require 'lsp-python-ms)(lsp-deferred))))
+              python-indent-guess-indent-offset nil)
 
 (provide 'lang-python)
