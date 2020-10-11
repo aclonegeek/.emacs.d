dab598bdd5fa13e9c1c6409571c4b43f825eecce
Randy Taylor <tehgecKozzz@gmail.com>
2020-10-10 22:23:16 -0400

    Set projectile-completion-system 'default

--- lisp/pkg-projectile.el
+++ lisp/pkg-projectile.el
@@ -7,14 +7,8 @@
   :config
   (setq projectile-enable-caching    t
         projectile-indexing-method   'alien
-        projectile-completion-system #'icomplete-projectile-vertical-completion
+        projectile-completion-system 'default
         projectile-generic-command   "fd . -0")
   (add-to-list 'projectile-globally-ignored-directories ".clangd"))
-
-(defun icomplete-projectile-vertical-completion (prompt choices)
-  "Provide vertical completion for projectile."
-  (interactive)
-  (icomplete-vertical-do ()
-    (completing-read prompt choices)))
 
 (provide 'pkg-projectile)
