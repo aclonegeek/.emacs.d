b8540477925db851ed4c4c3f6945bdb47ba35232
Randy Taylor <tehgecKozzz@gmail.com>
2021-03-08 21:19:05 -0500

    Set flyspell-issue-{welcome,message}-flag to nil

--- lisp/pkg-flyspell.el
+++ lisp/pkg-flyspell.el
@@ -4,6 +4,8 @@
   :hook (org-mode . flyspell-mode)
   :config
   (setq ispell-program-name "aspell"
-        ispell-dictionary   "english"))
+        ispell-dictionary   "english"
+        flyspell-issue-welcome-flag nil
+        flyspell-issue-message-flag nil))
 
 (provide 'pkg-flyspell)
