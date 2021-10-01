f1e923bd3759434f46afe79561a0b690bf512002
Randy Taylor <tehgecKozzz@gmail.com>
2021-09-30 21:52:01 -0400

    Disable {menu-scroll-tool}-bar-modes

--- early-init.el
+++ early-init.el
@@ -40,6 +40,9 @@
 (push '(menu-bar-lines . 0)   default-frame-alist)
 (push '(tool-bar-lines . 0)   default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+(setq menu-bar-mode   nil
+      scroll-bar-mode nil
+      tool-bar-mode   nil)
 
 ;; Ignore X resources.
 (advice-add #'x-apply-session-resources :override #'ignore)
