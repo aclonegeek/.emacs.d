733283b1af06624b769c92af66a9081800e605f3
Randy Taylor <tehgecKozzz@gmail.com>
2021-10-01 22:45:49 -0400

    Remove elisp-flymake-checkdoc hook in emacs-lisp-mode

--- lisp/pkg-flymake.el
+++ lisp/pkg-flymake.el
@@ -4,7 +4,10 @@
   :bind (("C-\\" . flymake-show-diagnostics-buffer)
          ("M-["  . flymake-goto-prev-error)
          ("M-]"  . flymake-goto-next-error))
-  :hook (emacs-lisp-mode . flymake-mode)
+  :hook ((emacs-lisp-mode . flymake-mode)
+         (emacs-lisp-mode . (lambda ()
+                              (remove-hook 'flymake-diagnostic-functions
+                                           'elisp-flymake-checkdoc t))))
   :config
   (setq flymake-no-changes-timeout nil)
   (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake))
