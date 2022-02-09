e9434549903b6de29a7dc23954bdb91967639bd8
Randy Taylor <tehgecKozzz@gmail.com>
2022-02-08 21:49:47 -0500

    Use xref-show-definitions-completing-read

--- lisp/pkg-xref.el
+++ lisp/pkg-xref.el
@@ -2,6 +2,8 @@
 
 (use-package xref
   :config
-  (setq xref-search-program 'ripgrep))
+  (setq xref-search-program 'ripgrep
+        xref-show-xrefs-function #'xref-show-definitions-completing-read
+        xref-show-definitions-function #'xref-show-definitions-completing-read))
 
 (provide 'pkg-xref)
