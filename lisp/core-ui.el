0dad7713efb2f80909271b5568ff2b46c7392df6
Randy Taylor <tehgecKozzz@gmail.com>
2022-02-28 10:30:15 -0500

    Set bookmark-set-fringe-mark nil

--- lisp/core-ui.el
+++ lisp/core-ui.el
@@ -5,6 +5,9 @@
 (blink-cursor-mode 0)            ;; Disable blinking.
 (set-default 'cursor-type 'box)  ;; Fat cursor.
 
+(defvar bookmark-set-fringe-mark)
+(setq bookmark-set-fringe-mark nil)
+
 (defvar show-paren-delay)
 (defvar show-paren-when-point-in-periphery)
 (setq show-paren-delay 0
