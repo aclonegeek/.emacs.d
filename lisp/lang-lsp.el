f4d8d5bf761ab7005a3494689830c4fde073c686
Randy Taylor <dev@rjt.dev>
2022-04-21 10:06:36 -0400

    Show all eldoc feedback (flymake feedback first) when using eglot

--- lisp/lang-lsp.el
+++ lisp/lang-lsp.el
@@ -5,7 +5,16 @@
   :bind (:map eglot-mode-map
               ("C-c l a" . eglot-code-actions)
               ("C-c l r" . eglot-rename))
-  :hook (((c-mode c++-mode go-mode) . eglot-ensure)))
+  :hook (((c-mode c++-mode go-mode) . eglot-ensure))
+  :config
+  (add-hook 'eglot-managed-mode-hook
+            (lambda ()
+              ;; Show flymake diagnostics first.
+              (setq eldoc-documentation-functions
+                    (cons #'flymake-eldoc-function
+                          (remove #'flymake-eldoc-function eldoc-documentation-functions)))
+              ;; Show all eldoc feedback.
+              (setq eldoc-documentation-strategy #'eldoc-documentation-compose))))
 
 
 (provide 'lang-lsp)
