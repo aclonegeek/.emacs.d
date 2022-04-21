4366dc58d7c0d97cf9a13c11a9ce0d46d4081213
Randy Taylor <dev@rjt.dev>
2022-04-21 09:22:37 -0400

    Remove custom company-backends

--- lisp/pkg-company.el
+++ lisp/pkg-company.el
@@ -5,8 +5,7 @@
   :commands company-mode
   :hook (prog-mode . company-mode)
   :config
-  (setq company-backends '((company-capf company-yasnippet) company-cmake company-files)
-        company-idle-delay 0.0
+  (setq company-idle-delay 0.0
         company-minimum-prefix-length 1
         company-tooltip-limit 10
         company-show-numbers 'left))
