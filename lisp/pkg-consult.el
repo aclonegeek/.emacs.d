3eb19fdb08a94b715aaeac497b5908381486171b
Randy Taylor <dev@rjt.dev>
2022-04-29 22:32:26 -0400

    Add another consult keybind

--- lisp/pkg-consult.el
+++ lisp/pkg-consult.el
@@ -5,6 +5,7 @@
          ("C-c k"   . consult-kmacro)
          ("C-x b"   . consult-buffer)
          ("C-x 4 b" . consult-buffer-other-window)
+         ("C-x 5 b" . consult-buffer-other-frame)
          ("C-x r b" . consult-bookmark)
          ("M-g e"   . consult-compile-error)
          ("M-g f"   . consult-flymake)
