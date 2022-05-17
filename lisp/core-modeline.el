4f088edd01f57ff0669be206a2597577ad02cfe8
Randy Taylor <dev@rjt.dev>
2022-05-16 22:33:19 -0400

    Support right-aligning items in modeline
    And align the flymake counters to the right.

--- lisp/core-modeline.el
+++ lisp/core-modeline.el
@@ -4,13 +4,30 @@
 (set-face-attribute 'mode-line-active nil :inherit 'mode-line)
 (set-face-attribute 'mode-line-inactive nil :inherit 'mode-line)
 
+(defvar mode-line-left)
+(setq mode-line-left
+      (list ""
+            'mode-line-directory
+            'mode-line-buffer-identification
+            " "
+            "%l:%c"))
+
+(defvar mode-line-right)
+(setq mode-line-right
+      (list ""
+            '(flymake-mode (:propertize ("/^v^\\" flymake-mode-line-counters)))))
+
 (setq-default mode-line-format
-              (list ""
-               'mode-line-directory
-               'mode-line-buffer-identification
-               " "
-               "%l:%c"
-               '(flymake-mode (:propertize ("/^v^\\" flymake-mode-line-counters)))))
+              (list
+               mode-line-left
+               '(:eval (mode-line-fill-right))
+               mode-line-right))
+
+(defun mode-line-fill-right ()
+  "Return enough empty space to right-align `mode-line-right' items."
+  (let ((length (length (format-mode-line mode-line-right))))
+        (propertize " "
+                    'display `(space :align-to (- right ,length)))))
 
 (defvar mode-line-directory
   '(:propertize
