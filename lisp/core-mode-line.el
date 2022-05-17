caa91ace6f285a3d443083a109702629ab1628f5
Randy Taylor <dev@rjt.dev>
2022-05-17 19:51:39 -0400

    Show buffer name without path in mode-line

--- lisp/core-mode-line.el
+++ lisp/core-mode-line.el
@@ -5,8 +5,7 @@
 (set-face-attribute 'mode-line-inactive nil :inherit 'mode-line)
 
 (defconst mode-line-left
-      (list ""
-            'mode-line-directory
+      (list " "
             'mode-line-buffer-identification
             " "
             "%l:%c"))
@@ -26,24 +25,5 @@
   (let ((length (length (format-mode-line mode-line-right))))
         (propertize " "
                     'display `(space :align-to (- right ,length)))))
-
-(defvar mode-line-directory
-  '(:propertize
-    (:eval
-     (when (buffer-file-name) (concat " " (shorten-directory default-directory 20))))))
-(put 'mode-line-directory 'risky-local-variable t)
-
-(defun shorten-directory (dir max-length)
-  "Show up to `max-length' characters of a directory name `dir'."
-  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
-               (output ""))
-       (when (and path (equal "" (car path)))
-         (setq path (cdr path)))
-       (while (and path (< (length output) (- max-length 4)))
-         (setq output (concat (car path) "/" output))
-         (setq path (cdr path)))
-       (when path
-         (setq output (concat ".../" output)))
-       output))
 
 (provide 'core-mode-line)
