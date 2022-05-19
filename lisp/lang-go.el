c927ba51b4d0e6d8c127327eee418598529d80e5
Randy Taylor <dev@rjt.dev>
2022-05-18 20:14:47 -0400

    Implement minor mode for go-mode using tree-sitter highlighting

--- lisp/lang-go.el
+++ lisp/lang-go.el
@@ -1,6 +1,134 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package go-mode
-  :hook (before-save . gofmt-before-save))
+  :hook ((go-mode . ts-hl-go-mode)
+         (before-save . gofmt-beforre-save)))
+
+(defvar go-font-lock-settings
+  '((go
+     (
+      ;; Comments.
+      (comment) @font-lock-comment-face
+
+      ;; Escape.
+      (escape_sequence) @font-lock-escape-face
+
+      ;; Constants.
+      [
+       (true)
+       (false)
+       (nil)
+       ] @font-lock-builtin-face
+
+      ;; Literals.
+      [
+       (interpreted_string_literal)
+       (raw_string_literal)
+       (rune_literal)
+       ] @font-lock-string-face
+
+      ;; Numbers.
+      [
+       (int_literal)
+       (float_literal)
+       (imaginary_literal)
+       ] @font-lock-number-face ;; Custom face.
+
+      ;; Identifiers.
+      (type_identifier) @font-lock-type-face
+      (field_identifier) @font-lock-property-face ;; Custom face.
+      (short_var_declaration left: (expression_list (identifier)) @font-lock-variable-name-face)
+      (var_declaration (var_spec name: (identifier) @font-lock-variable-name-face))
+      (parameter_declaration (identifier) @font-lock-variable-name-face)
+      (const_declaration (const_spec name: (identifier) @font-lock-constant-face))
+
+      ;; Functions calls.
+      (call_expression
+       function: (identifier) @font-lock-function-name-face)
+      (call_expression
+       function: (selector_expression
+                  field: (field_identifier)) @font-lock-function-name-face)
+      ;; Function definitions.
+      (function_declaration
+       name: (identifier) @font-lock-function-name-face)
+      (method_declaration
+       name: (field_identifier) @font-lock-function-name-face)
+
+      ;; Keywords.
+      [
+       "break"
+       "case"
+       "chan"
+       "const"
+       "continue"
+       "default"
+       "defer"
+       "else"
+       "fallthrough"
+       "for"
+       "func"
+       "go"
+       "goto"
+       "if"
+       "import"
+       "interface"
+       "map"
+       "package"
+       "range"
+       "return"
+       "select"
+       "struct"
+       "switch"
+       "type"
+       "var"
+       ] @font-lock-keyword-face
+
+      ;; Operators.
+      [
+       "--"
+       "-"
+       "-="
+       ":="
+       "!"
+       "!="
+       "..."
+       "*"
+       "*"
+       "*="
+       "/"
+       "/="
+       "&"
+       "&&"
+       "&="
+       "%"
+       "%="
+       "^"
+       "^="
+       "+"
+       "++"
+       "+="
+       "<-"
+       "<"
+       "<<"
+       "<<="
+       "<="
+       "="
+       "=="
+       ">"
+       ">="
+       ">>"
+       ">>="
+       "|"
+       "|="
+       "||"
+       ] @font-lock-operator-face ;; Custom face.
+         ))))
+
+(define-minor-mode ts-hl-go-mode
+  "Minor mode for highlighting Go using tree-sitter."
+  :lighter nil
+  (require 'treesit)
+  (setq-local treesit-font-lock-defaults '((go-font-lock-settings)))
+  (treesit-font-lock-enable))
 
 (provide 'lang-go)
