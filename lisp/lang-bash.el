bd39fd261a419d237e0f9943a09f422036009a9e
Randy Taylor <dev@rjt.dev>
2022-08-21 16:12:55 -0400

    Compile tree-sitter queries

--- lisp/lang-bash.el
+++ lisp/lang-bash.el
@@ -1,76 +1,78 @@
 ;; -*- lexical-binding: t; -*-
 
 (defvar bash-font-lock-settings
-  '((bash
-     (
-      ;; Comments.
-      (comment) @font-lock-comment-face
+  `((bash
+     ,(treesit-query-compile
+       'bash
+       '(
+         ;; Comments.
+         (comment) @font-lock-comment-face
 
-      (file_descriptor) @font-lock-number-face ;; Custom face.
+         (file_descriptor) @font-lock-number-face ;; Custom face.
 
-      (case_item
-       value: (word) @font-lock-constant-face)
+         (case_item
+          value: (word) @font-lock-constant-face)
 
-      [
-       (string)
-       (raw_string)
-       (heredoc_body)
-       (heredoc_start)
-       ] @font-lock-string-face
+         [
+          (string)
+          (raw_string)
+          (heredoc_body)
+          (heredoc_start)
+          ] @font-lock-string-face
 
-      [
-       (command_substitution)
-       (process_substitution)
-       (expansion)
-       ] @default
+         [
+          (command_substitution)
+          (process_substitution)
+          (expansion)
+          ] @default
 
-      (variable_name) @font-lock-property-face ;; Custom face.
+         (variable_name) @font-lock-property-face ;; Custom face.
 
-      (command_name) @font-lock-function-name-face
-      (function_definition
-       name: (word) @font-lock-function-name-face)
+         (command_name) @font-lock-function-name-face
+         (function_definition
+          name: (word) @font-lock-function-name-face)
 
-      ;; Keywords.
-      [
-       "case"
-       "do"
-       "done"
-       "elif"
-       "else"
-       "esac"
-       "export"
-       "fi"
-       "for"
-       "function"
-       "if"
-       "in"
-       "unset"
-       "while"
-       "then"
-       ] @font-lock-keyword-face
+         ;; Keywords.
+         [
+          "case"
+          "do"
+          "done"
+          "elif"
+          "else"
+          "esac"
+          "export"
+          "fi"
+          "for"
+          "function"
+          "if"
+          "in"
+          "unset"
+          "while"
+          "then"
+          ] @font-lock-keyword-face
 
-      ;; Operators.
-      [
-       "$"
-       "||"
-       "&&"
-       ">"
-       ">>"
-       "<"
-       "<<"
-       "<<<"
-       "<<-"
-       "=="
-       "!="
-       ";"
-       ";;"
-       ";&"
-       ";;&"
-       "|&"
-       "|"
-       (expansion_flags)
-       ] @font-lock-operator-face ;; Custom face.
-      ))))
+         ;; Operators.
+         [
+          "$"
+          "||"
+          "&&"
+          ">"
+          ">>"
+          "<"
+          "<<"
+          "<<<"
+          "<<-"
+          "=="
+          "!="
+          ";"
+          ";;"
+          ";&"
+          ";;&"
+          "|&"
+          "|"
+          (expansion_flags)
+          ] @font-lock-operator-face ;; Custom face.
+         )))))
 
 (define-minor-mode ts-hl-bash-mode
   "Minor mode for highlighting Bash using tree-sitter."
--- lisp/lang-go.el
+++ lisp/lang-go.el
@@ -5,129 +5,133 @@
          (before-save . gofmt-before-save)))
 
 (defvar go-font-lock-settings
-  '((go
-     (
-      ;; Comments.
-      (comment) @font-lock-comment-face
+  `((go
+     ,(treesit-query-compile
+       'go
+       '(
+         ;; Comments.
+         (comment) @font-lock-comment-face
 
-      ;; Escape.
-      (escape_sequence) @font-lock-escape-face
+         ;; Escape.
+         (escape_sequence) @font-lock-escape-face
 
-      ;; Constants.
-      [
-       (true)
-       (false)
-       (nil)
-       ] @font-lock-builtin-face
+         ;; Constants.
+         [
+          (true)
+          (false)
+          (nil)
+          ] @font-lock-builtin-face
 
-      ;; Literals.
-      [
-       (interpreted_string_literal)
-       (raw_string_literal)
-       (rune_literal)
-       ] @font-lock-string-face
+         ;; Literals.
+         [
+          (interpreted_string_literal)
+          (raw_string_literal)
+          (rune_literal)
+          ] @font-lock-string-face
 
-      ;; Numbers.
-      [
-       (int_literal)
-       (float_literal)
-       (imaginary_literal)
-       ] @font-lock-number-face ;; Custom face.
+         ;; Numbers.
+         [
+          (int_literal)
+          (float_literal)
+          (imaginary_literal)
+          ] @font-lock-number-face ;; Custom face.
 
-      ;; Identifiers.
-      (type_identifier) @font-lock-type-face
-      (field_identifier) @font-lock-property-face ;; Custom face.
-      (short_var_declaration left: (expression_list (identifier)) @font-lock-variable-name-face)
-      (var_declaration (var_spec name: (identifier) @font-lock-variable-name-face))
-      (parameter_declaration (identifier) @font-lock-variable-name-face)
-      (const_declaration (const_spec name: (identifier) @font-lock-constant-face))
+         ;; Identifiers.
+         (type_identifier) @font-lock-type-face
+         (field_identifier) @font-lock-property-face ;; Custom face.
+         (short_var_declaration left: (expression_list (identifier)) @font-lock-variable-name-face)
+         (var_declaration (var_spec name: (identifier) @font-lock-variable-name-face))
+         (parameter_declaration (identifier) @font-lock-variable-name-face)
+         (const_declaration (const_spec name: (identifier) @font-lock-constant-face))
 
-      ;; Function calls.
-      (call_expression
-       function: (identifier) @font-lock-function-name-face)
-      (call_expression
-       function: (selector_expression
-                  field: (field_identifier)) @font-lock-function-name-face)
-      ;; Function definitions.
-      (function_declaration
-       name: (identifier) @font-lock-function-name-face)
-      (method_declaration
-       name: (field_identifier) @font-lock-function-name-face)
+         ;; Function calls.
+         (call_expression
+          function: (identifier) @font-lock-function-name-face)
+         (call_expression
+          function: (selector_expression
+                     field: (field_identifier)) @font-lock-function-name-face)
+         ;; Function definitions.
+         (function_declaration
+          name: (identifier) @font-lock-function-name-face)
+         (method_declaration
+          name: (field_identifier) @font-lock-function-name-face)
 
-      ;; Keywords.
-      [
-       "break"
-       "case"
-       "chan"
-       "const"
-       "continue"
-       "default"
-       "defer"
-       "else"
-       "fallthrough"
-       "for"
-       "func"
-       "go"
-       "goto"
-       "if"
-       "import"
-       "interface"
-       "map"
-       "package"
-       "range"
-       "return"
-       "select"
-       "struct"
-       "switch"
-       "type"
-       "var"
-       ] @font-lock-keyword-face
+         ;; Keywords.
+         [
+          "break"
+          "case"
+          "chan"
+          "const"
+          "continue"
+          "default"
+          "defer"
+          "else"
+          "fallthrough"
+          "for"
+          "func"
+          "go"
+          "goto"
+          "if"
+          "import"
+          "interface"
+          "map"
+          "package"
+          "range"
+          "return"
+          "select"
+          "struct"
+          "switch"
+          "type"
+          "var"
+          ] @font-lock-keyword-face
 
-      ;; Operators.
-      [
-       "--"
-       "-"
-       "-="
-       ":="
-       "!"
-       "!="
-       "..."
-       "*"
-       "*"
-       "*="
-       "/"
-       "/="
-       "&"
-       "&&"
-       "&="
-       "%"
-       "%="
-       "^"
-       "^="
-       "+"
-       "++"
-       "+="
-       "<-"
-       "<"
-       "<<"
-       "<<="
-       "<="
-       "="
-       "=="
-       ">"
-       ">="
-       ">>"
-       ">>="
-       "|"
-       "|="
-       "||"
-       ] @font-lock-operator-face ;; Custom face.
-         ))))
+         ;; Operators.
+         [
+          "--"
+          "-"
+          "-="
+          ":="
+          "!"
+          "!="
+          "..."
+          "*"
+          "*"
+          "*="
+          "/"
+          "/="
+          "&"
+          "&&"
+          "&="
+          "%"
+          "%="
+          "^"
+          "^="
+          "+"
+          "++"
+          "+="
+          "<-"
+          "<"
+          "<<"
+          "<<="
+          "<="
+          "="
+          "=="
+          ">"
+          ">="
+          ">>"
+          ">>="
+          "|"
+          "|="
+          "||"
+          ] @font-lock-operator-face ;; Custom face.
+         )))))
 
 (define-minor-mode ts-hl-go-mode
   "Minor mode for highlighting Go using tree-sitter."
   :lighter nil
   (require 'treesit)
+  (unless font-lock-defaults
+    (setq font-lock-defaults '(nil t)))
   (setq-local treesit-font-lock-defaults '((go-font-lock-settings)))
   (treesit-font-lock-enable))
 
