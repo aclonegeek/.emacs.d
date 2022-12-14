7e57f4b33e3ef02c971638b11d40fdb22b425a89
Randy Taylor <dev@rjt.dev>
2022-12-14 14:13:00 -0500

    Fix magit option in project pop-up after recent project changes

--- lisp/core-project.el
+++ lisp/core-project.el
@@ -2,14 +2,16 @@
 
 (use-package project
   :straight (:type built-in)
-  :bind (:map project-prefix-map
-              ("m" . magit-status))
+  :defer t
   :config
   (setq project-switch-commands '((project-find-file "File")
                                   (project-find-dir "Directory")
                                   (project-find-regexp "Regexp")
                                   (project-query-replace-regexp "Query replace")
                                   (project-eshell "Eshell")
-                                  (magit-status "Magit"))))
+                                  ((lambda ()
+                                     (interactive)
+                                     (magit-status project-current-directory-override))
+                                   "Magit" ?m))))
 
 (provide 'core-project)
