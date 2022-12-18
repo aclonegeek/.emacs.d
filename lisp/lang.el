62f9599300b17aabb11cc58f4f62408cb1d9dbdf
Randy Taylor <dev@rjt.dev>
2022-12-17 22:29:39 -0500

    Format buffers on save

--- lisp/lang.el
+++ lisp/lang.el
@@ -1,11 +1,52 @@
 ;; -*- lexical-binding: t; -*-
 
+(defun rjt-format-buffer (command)
+  "Format the buffer with COMMAND."
+  (let ((point (point)))
+    (shell-command-on-region
+     (point-min)
+     (point-max)
+     command
+     (current-buffer)
+     t
+     "*Format Buffer Errors"
+     t)
+    (goto-char point)))
+
+(defun rjt-format-c/c++-buffer ()
+  "Format a C/C++ buffer."
+  (interactive)
+  (rjt-format-buffer "clang-format"))
+
+(defun rjt-format-go-buffer ()
+  "Format a Go buffer."
+  (interactive)
+  (rjt-format-buffer "gofmt"))
+
+(defun rjt-format-rust-buffer ()
+  "Format a Rust buffer."
+  (interactive)
+  (rjt-format-buffer "rustfmt"))
+
 ;; C.
 (setq-default c-basic-offset 4)
-;; TODO(rtaylor): Format buffer on save.
+(add-hook 'c-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook #'rjt-format-c/c++-buffer nil 'local)))
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook #'rjt-format-c/c++-buffer nil 'local)))
+(add-hook 'c-ts-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook #'rjt-format-c/c++-buffer nil 'local)))
+(add-hook 'c++-ts-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook #'rjt-format-c/c++-buffer nil 'local)))
 
 ;; Go.
-;; TODO(rtaylor): Format buffer on save.
+(add-hook 'go-ts-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook #'rjt-format-go-buffer nil 'local)))
 
 ;; Markdown.
 (use-package markdown-mode
@@ -21,6 +62,8 @@
               python-indent-guess-indent-offset nil)
 
 ;; Rust.
-;; TODO(rtaylor): Format buffer on save.
+(add-hook 'go-ts-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook #'rjt-format-rust-buffer nil 'local)))
 
 (provide 'lang)
