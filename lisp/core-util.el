8666a6dd778d2e905a734c4ea2596d5704d28f1b
Randy Taylor <dev@rjt.dev>
2024-01-23 21:29:36 -0500

    Simplify rjt-calc-eval-region

--- lisp/core-util.el
+++ lisp/core-util.el
@@ -38,32 +38,12 @@
   (interactive)
   (find-file (concat user-emacs-directory "init.el")))
 
-; https://www.reddit.com/r/emacs/comments/aja311/first_trial_of_a_weekly_emacs_tipstricksetc_thread/eev3iu4/
-(defun rjt-calc-eval-region (arg)
-  "Evaluate an expression in calc and communicate the result.
-
-If the region is active then evaluate it, otherwise search backwards
-to the first whitespace character to find the beginning of the
-expression. By default, replace the expression with its value. If
-called with the universal prefix argument, keep the expression
-and insert the result into the buffer after it. If called with a
-negative prefix argument, just echo the result in the
-minibuffer."
-  (interactive "p")
-  (let (start end)
-    (if (use-region-p)
-        (setq start (region-beginning) end (region-end))
-      (setq end (point))
-      (setq start (search-backward-regexp "\\s-\\|\n" 0 1))
-      (setq start (1+ (if start start 0)))
-      (goto-char end))
-    (let ((value (calc-eval (buffer-substring-no-properties start end))))
-      (pcase arg
-        (1 (delete-region start end))
-        (4 (insert " = ")))
-      (pcase arg
-        ((or 1 4) (insert value))
-        (-1 (message value))))))
+(defun rjt-calc-eval-region (start end)
+  "Evaluate a region with calc."
+  (interactive "r")
+  (let ((region (buffer-substring-no-properties start end)))
+    (delete-region start end)
+    (insert (calc-eval region))))
 
 (defun rjt-sudo-edit ()
   "Edit the current file with sudo."
