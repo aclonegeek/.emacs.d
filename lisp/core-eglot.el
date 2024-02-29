3f4aeb4f1fa7432307eebe69ad0057f3d7026f08
Randy Taylor <dev@rjt.dev>
2024-02-28 21:50:07 -0500

    Use eglot-events-buffer-config instead of eglot-events-buffer-size

--- lisp/core-eglot.el
+++ lisp/core-eglot.el
@@ -6,10 +6,10 @@
               ("C-c l a" . eglot-code-actions)
               ("C-c l r" . eglot-rename))
   :config
-  (setq eglot-events-buffer-size 0
-        eglot-sync-connect 0
+  (setq eglot-sync-connect 0
         eglot-ignored-server-capabilities '(:documentFormattingProvider
                                             :documentOnTypeFormattingProvider))
+  (plist-put eglot-events-buffer-config :size 0)
   (add-to-list 'eglot-stay-out-of 'imenu)
   (add-to-list 'eglot-server-programs
                '((c-ts-mode c++-ts-mode) . ("clangd"
