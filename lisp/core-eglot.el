3141cc28273cdb8deb9216ee4a0a431e429567c0
Randy Taylor <dev@rjt.dev>
2024-04-03 22:24:13 -0400

    Fix Flymake diagnostics being incorrect/out of date when using Eglot

--- lisp/core-eglot.el
+++ lisp/core-eglot.el
@@ -5,6 +5,17 @@
   :bind (:map eglot-mode-map
               ("C-c l a" . eglot-code-actions)
               ("C-c l r" . eglot-rename))
+  ;; Flymake reporting will be incorrect/out of date because we set
+  ;; flymake-no-changes-timeout to nil and
+  ;; flymake-start-on-save-buffer to t. This causes Flymake to ask for
+  ;; diagnostics on a save but the server hasn't yet published the
+  ;; notification that says there are no diagnostics at all (see
+  ;; https://github.com/joaotavora/eglot/issues/1296). To work around
+  ;; this, we let Eglot run Flymake when it has diagnostics and
+  ;; prevent Flymake from running otherwise.
+  :hook (eglot-managed-mode . (lambda ()
+                                (setq-local flymake-no-changes-timeout most-positive-fixnum
+                                            flymake-start-on-save-buffer nil)))
   :config
   (setq eglot-autoshutdown t
         eglot-report-progress nil
