591d9a8f7c0b9ab8660596449cc4a91a1b455cbe
Randy Taylor <dev@rjt.dev>
2024-07-02 22:27:30 -0400

    Remove buffer formatting code
    Don't need it right now and there is an annoying
    bug when multiple windows with the same buffer are
    open and formatting is applied: all other windows
    except the current window have point moved to the
    start of the buffer.

--- lisp/lang.el
+++ lisp/lang.el
@@ -1,33 +1,5 @@
 ;; -*- lexical-binding: t; -*-
 
-(defun rjt-format-buffer (command)
-  "Format the buffer with COMMAND."
-  (let ((point (point)))
-    (shell-command-on-region
-     (point-min)
-     (point-max)
-     command
-     (current-buffer)
-     t
-     "*Format Buffer Errors"
-     t)
-    (goto-char point)))
-
-(defun rjt-format-c/c++-buffer ()
-  "Format a C/C++ buffer."
-  (interactive)
-  (rjt-format-buffer "clang-format"))
-
-(defun rjt-format-go-buffer ()
-  "Format a Go buffer."
-  (interactive)
-  (rjt-format-buffer "gofmt"))
-
-(defun rjt-format-rust-buffer ()
-  "Format a Rust buffer."
-  (interactive)
-  (rjt-format-buffer "rustfmt"))
-
 ;; tree-sitter.
 (setq treesit-language-source-alist
    '((bash "https://github.com/tree-sitter/tree-sitter-bash")
@@ -58,23 +30,6 @@
 
 ;; C.
 (setq-default c-basic-offset 4)
-(add-hook 'c-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook #'rjt-format-c/c++-buffer nil 'local)))
-(add-hook 'c++-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook #'rjt-format-c/c++-buffer nil 'local)))
-(add-hook 'c-ts-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook #'rjt-format-c/c++-buffer nil 'local)))
-(add-hook 'c++-ts-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook #'rjt-format-c/c++-buffer nil 'local)))
-
-;; Go.
-(add-hook 'go-ts-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook #'rjt-format-go-buffer nil 'local)))
 
 ;; Markdown.
 (use-package markdown-mode
@@ -88,10 +43,5 @@
 (setq-default python-shell-interpreter "python3"
               python-indent-offset 4
               python-indent-guess-indent-offset nil)
-
-;; Rust.
-(add-hook 'rust-ts-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook #'rjt-format-rust-buffer nil 'local)))
 
 (provide 'lang)
