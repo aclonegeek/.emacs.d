789369b68090527664b4884ccaadd8e141d0bf3f
Randy Taylor <dev@rjt.dev>
2024-07-07 21:48:45 -0400

    Use mode-line-format-right-align

--- lisp/core-mode-line.el
+++ lisp/core-mode-line.el
@@ -4,32 +4,21 @@
 (set-face-attribute 'mode-line-active nil :inherit 'mode-line)
 (set-face-attribute 'mode-line-inactive nil :inherit 'mode-line)
 
-(defconst mode-line-left
-  (list " "
-        '(:eval (mode-line-project-name))
-        'mode-line-buffer-identification
-        " "
-        "%l:%c"))
-
-(defconst mode-line-right
-  (list ""
-        '(flymake-mode (:propertize ("/^v^\\" flymake-mode-line-counters)))))
-
 (setq-default mode-line-format
               (list
-               mode-line-left
-               '(:eval (mode-line-fill-right))
-               mode-line-right))
-
-(defun mode-line-fill-right ()
-  "Return enough empty space to right-align `mode-line-right' items."
-  (let ((length (length (format-mode-line mode-line-right))))
-    (propertize " "
-                'display `(space :align-to (- right ,length)))))
+               " "
+               '(:eval (mode-line-project-name))
+               'mode-line-buffer-identification
+               " "
+               "%l:%c"
+               'mode-line-format-right-align
+               '(flymake-mode (:propertize ("/^v^\\" flymake-mode-line-counters)))
+               " "
+               ))
 
 (defun mode-line-project-name ()
   "Return the project name for the project formatted for the mode-line."
-  (if-let ((proj (project-current)))
+  (when-let ((proj (project-current)))
       (list "["
             (project-name proj)
             "] ")))
