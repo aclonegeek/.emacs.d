9e9878c7e024ae4d6e7669f479f1bc2ce530f568
Randy Taylor <dev@rjt.dev>
2025-04-12 20:39:28 -0400

    Replace ef-themes with miasma

--- lisp/core-ui.el
+++ lisp/core-ui.el
@@ -27,7 +27,7 @@
                                 :family "Iosevka"
                                 :height 110)))
 
-(use-package ef-themes)
+(use-package miasma-theme)
 
 (with-eval-after-load 'shr
   (set-face-attribute 'shr-text nil :inherit nil)
@@ -35,7 +35,7 @@
 
 ;; Theme
 (add-hook 'after-init-hook (lambda ()
-                             (load-theme 'ef-night t)))
+                             (load-theme 'miasma t)))
 
 ;; Modeline
 (use-package core-mode-line
