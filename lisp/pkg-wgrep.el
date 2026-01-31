df606221022f2423766789ecb5c1ce2ebf8f9d31
Randy Taylor <dev@rjt.dev>
2026-01-31 17:14:14 -0500

    Remove wgrep

--- lisp/pkg-wgrep.el
+++
@@ -1,9 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-(use-package wgrep
-  :bind (("C-x C-q" . wgrep-change-to-wgrep-mode)
-         ("C-c C-c" . wgrep-finish-edit))
-  :config
-  (setq wgrep-auto-save-buffer t))
-
-(provide 'pkg-wgrep)
